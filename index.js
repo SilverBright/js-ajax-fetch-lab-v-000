156bf8b9c1a7c36d132deb61162b2ead537dd71d
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-05 01:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ function createIssue() {
       title: document.getElementById('title').value,
       body: document.getElementById('body').value
 }
+fetch(url, {
+   method: 'POST',
+   body: JSON.stringify(postData),
+   headers: {
+    Authorization: `token ${getToken()}`
+  }
+  }).then(res => res.json()).then(json => getIssues());
+}
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
